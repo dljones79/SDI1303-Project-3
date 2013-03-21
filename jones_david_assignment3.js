da4275883c1db20406f9e5e4b889936b88840c12
David Jones <dljones79@fullsail.edu>
2013-03-21 15:19:30 -0500

    Added global variables and JSON data.

--- jones_david_assignment3.js
+++ jones_david_assignment3.js
@@ -1,1 +1,57 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// David Jones
+// SDI 1303
+// Project 3
+
+//Global Variables
+var groupSize = 6,
+	hitPoints = 27000,
+	mobName = "Avatar of Fear",
+	loot1 = "Coif of Enlivened Chance",
+	loot2 = "Bangle of Demise",
+	loot3 = "Corrupted Plate Greaves of Sorrow"
+; // End of Global Variables
+
+// JSON Data
+var players = {
+	"groupMembers": {
+		"1": {
+			"name": "Attritank",
+			"class": "Monk"
+		},
+		"2": {
+			"name": "Attricane",
+			"class": "Ranger"
+		},
+		"3": {
+			"name": "Trevail",
+			"class": "Fury"
+		},
+		"4": {
+			"name": "Royale",
+			"class": "Dirge"
+		},
+		"5": {
+			"name": "Mezzem",
+			"class": "Illusionist"
+		},
+		"6": {
+			"name": "Kataklysm",
+			"class": "Warlock"
+		}
+	} //End of groupMembers object
+}; // End of players Variable (JSON)
+
+
+
+
+
+/*
+
+for (var key in players.groupMembers){
+	var member = players.groupMembers[key];
+	console.log("Hello " + member.name + ".");
+};
+
+*/
